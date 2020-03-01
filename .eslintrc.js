05ac224fd28ca17e267fd8660644d676b40f8aa2
Fir3st <fojtik.v@gmail.com>
2020-03-01 06:16:02 +0100

    Some changes for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,25 @@ module.exports = {
         browser: true,
         node: true
     },
-    extends: [
-        '@nuxtjs/eslint-config-typescript',
-        'plugin:vue/recommended',
-        'plugin:prettier/recommended'
-    ],
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:vue/recommended', 'plugin:prettier/recommended'],
     // required to lint *.vue files
-    plugins: [
-        'vue'
-    ],
+    plugins: ['vue'],
     // add your custom rules here
     rules: {
-        'semi': [2, 'always'],
+        semi: [2, 'always'],
         'no-console': 'off',
         'vue/max-attributes-per-line': 'off',
-        'prettier/prettier': ['error', { 'semi': true }],
-        'indent': 'off',
+        'prettier/prettier': ['error', { semi: true }],
+        indent: 'off',
         'vue/html-indent': ['error', 4],
-        'vue/script-indent': ['error', 4, {
-            "baseIndent": 1,
-            "switchCase": 1
-        }],
+        'vue/script-indent': [
+            'error',
+            4,
+            {
+                baseIndent: 1,
+                switchCase: 1
+            }
+        ],
         'require-await': 'off'
     }
 };
